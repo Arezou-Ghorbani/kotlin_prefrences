a08d0734bfd6679adf487f88e6f3b3deded07d82
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-11-25 13:42:14 +0330

    room implementation in android kotlin

---
+++ s1-room/src/main/java/arezou/ghorbani/s1_room/db/UserDatabase.kt
@@ -0,0 +1,9 @@
+package arezou.ghorbani.s1_room.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [UserEntity::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+    abstract fun dao(): UserDao
+}
